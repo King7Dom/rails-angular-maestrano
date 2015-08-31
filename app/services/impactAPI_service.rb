687de10ea2f1484bb48a884d50414604524052d8
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-08-31 22:26:07 +1000

    Refactor ImpactAPIService to resemble a Singleton Pattern

--- app/services/impactAPI_service.rb
+++ app/services/impactAPI_service.rb
@@ -1,51 +1,40 @@
 require 'rest-client'
 
 module ImpactAPIService
+  class << self
 
-  PROTOCOL = 'https'
-  BASE_URL = 'api-impac-uat.maestrano.io/api/v1/get_widget'
-  USERNAME = '72db99d0-05dc-0133-cefe-22000a93862b'
-  PASSWORD = '_cIOpimIoDi3RIviWteOTA'
+    PROTOCOL = 'https'
+    BASE_URL = 'api-impac-uat.maestrano.io/api/v1/get_widget'
+    USERNAME = '72db99d0-05dc-0133-cefe-22000a93862b'
+    PASSWORD = '_cIOpimIoDi3RIviWteOTA'
 
-  def self.employee_list
-    url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employees_list&metadata[organization_ids][]=org-fbte"
-    puts "Requesting url: #{url}"
-    response = RestClient::Request.execute(
-      url: url,
-      user: USERNAME,
-      password: PASSWORD,
-      method: :get,
-      accept: :json,
-      verify_ssl: false
-    )
-    return response
-  end
+    def employee_list
+      url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employees_list&metadata[organization_ids][]=org-fbte"
+      return request_impact_api url
+    end
 
-  def self.employee_detail
-    url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employee_details&metadata[organization_ids][]=org-fbte"
-    puts "Requesting url: #{url}"
-    response = RestClient::Request.execute(
-      url: url,
-      user: USERNAME,
-      password: PASSWORD,
-      method: :get,
-      accept: :json,
-      verify_ssl: false
-    )
-    return response
-  end
+    def employee_detail
+      url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employee_details&metadata[organization_ids][]=org-fbte"
+      return request_impact_api url
+    end
 
-  def self.invoice_list
-    url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employee_details&metadata[organization_ids][]=org-fbte&metadata[entity]=customers|suppliers"
-    puts "Requesting url: #{url}"
-    response = RestClient::Request.execute(
-      url: url,
-      user: USERNAME,
-      password: PASSWORD,
-      method: :get,
-      accept: :json,
-      verify_ssl: false
-    )
-    return response
+    def invoice_list
+      url = "#{PROTOCOL}://#{BASE_URL}?engine=hr/employee_details&metadata[organization_ids][]=org-fbte&metadata[entity]=customers|suppliers"
+      return request_impact_api url
+    end
+
+    private
+    def request_impact_api(url)
+      puts "Requesting url: #{url}"
+      return RestClient::Request.execute(
+        url: url,
+        user: USERNAME,
+        password: PASSWORD,
+        method: :get,
+        accept: :json,
+        verify_ssl: false
+      )
+    end
+
   end
 end
