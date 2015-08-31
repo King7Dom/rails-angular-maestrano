0b6fa9bd81516fe8bc451929b8a211c251b22c7c
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-08-31 23:53:36 +1000

    EmployeesController#location parses the JSON response and return work locations

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -4,7 +4,17 @@ class Api::EmployeesController < ApplicationController
 
   def location
     response = ImpactAPIService.employee_list
-    render json: response
+    work_locations = Hash.new 0
+    for employee in response['content']['employees']
+      puts employee
+      for work_location in employee['work_locations']
+        unless work_location['id'].empty?
+          work_locations[work_location['id']] += 1
+        end
+      end
+    end
+
+    render json: work_locations
   end
 
   def detail
